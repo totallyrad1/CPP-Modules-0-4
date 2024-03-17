d287cff6a05dde6638f6d0fad8f2acc424c7368b
Asaad Naji <asnaji@e2r5p12.1337.ma>
2024-03-17 13:55:51 +0000

    fixed phonebook mistakes

---
+++ cpp00/ex01/Contact.cpp
@@ -0,0 +1,132 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asnaji <asnaji@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/17 13:51:36 by asnaji            #+#    #+#             */
+/*   Updated: 2024/03/17 13:54:44 by asnaji           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Phonebook.hpp"
+#include "Contact.hpp"
+
+int checkvalid(std::string str)
+{
+	int i = 0;
+	while(str[i] && str[i] >= '0' && str[i] <= '9')
+		i++;
+	if(!str[i])
+		return (0);
+	return (1);
+}
+
+void print_inside(std::string str)
+{
+	int x;
+	if(str.length() <= 9)
+	{
+		x = 0;
+		while(str.length() + x < 9)
+		{
+			std::cout << " ";
+			x++;
+		}
+		x = 0;
+		while(str[x])
+		{
+			std::cout << str[x];
+			x++;
+		}
+		std::cout << "|";
+	}
+	else
+	{
+		x = 0;
+		while(x < 8)
+		{
+			std::cout << str[x];
+			x++;
+		}
+		std::cout << ".|";
+	}
+}
+
+void Contact::rad_add(Phonebook *book)
+{
+	std::string firstname, lastname, nickname, darkest_secret, phtxt;
+	static int i;
+	if (i == 8)
+		i = 0;
+	std::cout << "\033[0;32menter contact phone number: \033[0m";
+	if(!std::getline(std::cin, phtxt) || std::cin.eof())
+		return ;
+	std::cout << "\033[0;32menter contact first name: \033[0m";
+	if(!std::getline(std::cin, firstname) || std::cin.eof())
+		return ;
+	std::cout << "\033[0;32menter contact last name: \033[0m";
+	if(!std::getline(std::cin, lastname) || std::cin.eof())
+		return ;
+	std::cout << "\033[0;32menter contact nickname: \033[0m";
+	if(!std::getline(std::cin, nickname) || std::cin.eof())
+		return ;
+	std::cout << "\033[0;32menter contact darkest secret: \033[0m";
+	if(!std::getline(std::cin, darkest_secret) || std::cin.eof())
+		return ;
+	if((checkvalid(phtxt) || phtxt.empty()) || firstname.empty() || lastname.empty() || nickname.empty() || darkest_secret.empty())
+	{
+		std::cout << "\033[0;31m          INVALID INPUT          \033[0m" << std::endl;
+		return ;
+	}
+	book->contacts[i].phone_number = phtxt;
+	book->contacts[i].firstname = firstname;
+	book->contacts[i].lastname = lastname;
+	book->contacts[i].nickname = nickname;
+	book->contacts[i].darkest_secret = darkest_secret;
+	i++;
+}
+
+void Contact::print_phonebook(Phonebook *book)
+{
+	int i = 0;
+	std::cout << " -------------------------------------- " << std::endl;
+	std::cout << "|  index |" << "firstname|" << " lastname|" << " nickname|" << std::endl;
+	while(i < 8)
+	{
+		std::cout << "|       " << i << "|";
+		print_inside(book->contacts[i].firstname);
+		print_inside(book->contacts[i].lastname);
+		print_inside(book->contacts[i].nickname);
+		std::cout << std::endl;
+		i++;
+	}
+	std::cout << " -------------------------------------- " << std::endl;
+}
+
+void Contact::rad_search(Phonebook *book)
+{
+	print_phonebook(book);
+	int index;
+	std::string intxt;
+	std::cout << "\033[0;32mcontact index ? \033[0m";
+	if(!std::getline(std::cin, intxt) || std::cin.eof())
+		return ;
+	if(checkvalid(intxt) || intxt.empty())
+	{
+		std::cout << "\033[0;31m          INVALID INPUT          \033[0m" << std::endl;
+		return ;
+	}
+	index = atoi(intxt.c_str());
+	if(index >= 0 && index <= 7 && book->contacts[index].firstname.empty())
+	{
+		std::cout << "\033[0;31m          INVALID INDEX          \033[0m" << std::endl;
+		return ;
+	}
+	std::cout << "\033[0;33mPhone number : \033[0m" << book->contacts[index].phone_number << std::endl;
+	std::cout << "\033[0;33mFirst name : \033[0m" << book->contacts[index].firstname << std::endl;
+	std::cout << "\033[0;33mLast name : \033[0m" << book->contacts[index].lastname << std::endl;
+	std::cout << "\033[0;33mNickname : \033[0m" << book->contacts[index].nickname << std::endl;
+	std::cout << "\033[0;33mdarkest secret : \033[0m" << book->contacts[index].darkest_secret << std::endl;
+}
--- cpp00/ex01/Contact.hpp
+++ cpp00/ex01/Contact.hpp
@@ -1,30 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   phonebook.hpp                                      :+:      :+:    :+:   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: asnaji <asnaji@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/14 14:28:33 by asnaji            #+#    #+#             */
-/*   Updated: 2024/03/17 13:54:06 by asnaji           ###   ########.fr       */
+/*   Created: 2024/03/17 13:48:41 by asnaji            #+#    #+#             */
+/*   Updated: 2024/03/17 13:54:38 by asnaji           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHONEBOOK_HPP
-# define PHONEBOOK_HPP
-# include <iostream>
-# include <cstdlib>
-# include <sstream>
-# include <iostream>
-# include <iomanip>
-#include "Contact.hpp"
-# define RAD 1
 
-class Phonebook{
-	public:
-		int oldest;
-		Contact contacts[8];	
+#ifndef CONTACT_HPP
+#define CONTACT_HPP
+#include "Phonebook.hpp"
+class Phonebook;
+class Contact{
+	public :
+		void rad_add(Phonebook *book);
+		void rad_search(Phonebook *book);
+		void print_phonebook(Phonebook *book);
+
+	private:
+		std::string firstname;
+		std::string lastname;
+		std::string nickname;
+		std::string phone_number;
+		std::string darkest_secret;
 };
-
 
 #endif
